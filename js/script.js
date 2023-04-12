4b159998711dc25ad632f346a6a08359115476c4
Pedro Romão <pedroferraz.romao@gmail.com>
2023-04-12 20:38:53 -0300

    atualizacoes de 12/04

--- js/script.js
+++ js/script.js
@@ -1,6 +1,48 @@
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(n) {
+  slides[currentSlide].classList.remove('active');
+  slides[n].classList.add('active');
+  slides[n].classList.add('next');
+  slides[currentSlide].classList.add('prev');
+  currentSlide = n;
+  setTimeout(() => {
+    slides[currentSlide].classList.remove('prev');
+    slides[n].classList.remove('next');
+  }, 1000);
+}
+
+function nextSlide() {
+  let next = currentSlide + 1;
+  if (next >= slides.length) {
+    next = 0;
+  }
+  showSlide(next);
+}
+
+setInterval(() => {
+  nextSlide();
+}, 5000);
+
+
+
+function showSlides() {
+  slideIndex++;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  slides.forEach(slide => slide.classList.remove('active'));
+  slides[slideIndex].classList.add('active');
+  setTimeout(showSlides, 3000);
+}
+
+showSlides();
+
+
+
 const botoes = document.querySelectorAll("#botoes > button");
 let botaoSelecionado = botoes[0];
-
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].addEventListener("click", function () {
@@ -9,3 +51,6 @@ for (let i = 0; i < botoes.length; i++) {
     botaoSelecionado.classList.add("selected");
   });
 }
+
+
+
